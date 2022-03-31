1a086a29c7a1c0681adef2bdcb3fd7223d135427
Ryan Levick <me@ryanlevick.com>
2022-03-31 11:32:37 +0200

    Clean up refactor

--- sync-team/src/zulip/mod.rs
+++ sync-team/src/zulip/mod.rs
@@ -1,3 +1,5 @@
+use std::collections::{BTreeMap, HashMap};
+
 use failure::Error;
 use reqwest::Client;
 use serde::Deserialize;
@@ -5,88 +7,102 @@ use serde::Deserialize;
 use crate::team_api::TeamApi;
 
 pub(crate) fn run(token: String, team_api: &TeamApi, dry_run: bool) -> Result<(), Error> {
-    let zulip = ZulipApi::new(token, dry_run);
-    let teams = team_api.get_teams()?;
-    let zulip_map = team_api.get_zulip_map()?;
-    let members = zulip.get_members()?;
-
-    // This maps from Zulip email to Zulip id
-    let email_map = {
-        let mut map = std::collections::BTreeMap::new();
-        for member in &members {
-            map.insert(member.email.clone(), member.user_id);
-        }
-        map
-    };
-
-    // This maps from Zulip name to Zulip id
-    let name_map = {
-        let mut map = std::collections::BTreeMap::new();
-        for member in members {
-            map.insert(member.name, member.user_id);
-        }
-        map
-    };
-
-    // The zulip map goes from zulip id to github id so we need to reverse it
-    let zulip_map = {
-        let mut map = std::collections::BTreeMap::new();
-        for (zulip_id, github_id) in &zulip_map.users {
-            map.insert(*github_id, *zulip_id);
-        }
-        map
-    };
-    for team in teams
+    let zulip_api = ZulipApi::new(token, dry_run);
+    let cache = ZulipCache::new(team_api, &zulip_api)?;
+    for team in cache
+        .teams()
         .iter()
         .filter(|t| matches!(t.kind, rust_team_data::v1::TeamKind::Team))
     {
-        let name = format!("T-{}", team.name);
         let mut ids = vec![];
         for member in &team.members {
-            let id = zulip_id_from_member(&name_map, &email_map, &zulip_map, member)?;
+            let id = cache.zulip_id_from_member(member)?;
             match id {
                 Some(id) => ids.push(id),
-                None => log::error!(
-                    "could not find id for {} {} {:?}",
+                None => log::warn!(
+                    "could not find id for {} ({} {:?})",
                     member.name,
                     member.github,
                     member.email
                 ),
             }
         }
-        zulip.create_user_group(&name, &ids)?;
-
-        // Add all team members to team
-        //   TODO: decide what should happen if there's no GitHub to Zulip mapping
-        //   TODO: decide if those in Zulip and not in team repo should be deleted
+        zulip_api.create_user_group(&team.name, &ids)?;
     }
     Ok(())
 }
 
-fn zulip_id_from_member(
-    name_map: &std::collections::BTreeMap<String, usize>,
-    email_map: &std::collections::BTreeMap<String, usize>,
-    zulip_map: &std::collections::BTreeMap<usize, usize>,
-    member: &rust_team_data::v1::TeamMember,
-) -> Result<Option<usize>, Error> {
-    if let Some(id) = zulip_map.get(&member.github_id) {
-        return Ok(Some(*id));
-    }
-    if let Some(id) = name_map.get(&member.github) {
-        return Ok(Some(*id));
-    }
-    if let Some(id) = name_map.get(&member.name) {
-        return Ok(Some(*id));
-    }
-
-    let email = match &member.email {
-        Some(e) => e,
-        None => return Ok(None),
-    };
-
-    Ok(email_map.get(email).copied())
+/// Caches data about teams and Zulip for easy and efficient lookup
+struct ZulipCache {
+    /// Map of Zulip user names to Zulip user ids
+    names: BTreeMap<String, usize>,
+    /// Map of Zulip emails to Zulip user ids
+    emails: BTreeMap<String, usize>,
+    /// Map of GitHub ids to Zulip user ids
+    github_ids: BTreeMap<usize, usize>,
+    /// Teams
+    teams: Vec<rust_team_data::v1::Team>,
 }
 
+impl ZulipCache {
+    fn new(team_api: &TeamApi, zulip_api: &ZulipApi) -> Result<Self, Error> {
+        let teams = team_api.get_teams()?;
+        let zulip_map = team_api.get_zulip_map()?;
+        let members = zulip_api.get_users()?;
+
+        let (names, emails) = {
+            let mut names = BTreeMap::new();
+            let mut emails = BTreeMap::new();
+            for member in members {
+                names.insert(member.name, member.user_id);
+                emails.insert(member.email, member.user_id);
+            }
+            (names, emails)
+        };
+
+        let github_ids = zulip_map
+            .users
+            .iter()
+            .map(|(zulip_id, github_id)| (*github_id, *zulip_id))
+            .collect();
+
+        Ok(Self {
+            teams,
+            names,
+            emails,
+            github_ids,
+        })
+    }
+
+    /// Get a Zulip user id for a Team member
+    fn zulip_id_from_member(
+        &self,
+        member: &rust_team_data::v1::TeamMember,
+    ) -> Result<Option<usize>, Error> {
+        if let Some(id) = self.github_ids.get(&member.github_id) {
+            return Ok(Some(*id));
+        }
+        if let Some(id) = self.names.get(&member.github) {
+            return Ok(Some(*id));
+        }
+        if let Some(id) = self.names.get(&member.name) {
+            return Ok(Some(*id));
+        }
+
+        let email = match &member.email {
+            Some(e) => e,
+            None => return Ok(None),
+        };
+
+        Ok(self.emails.get(email).copied())
+    }
+
+    fn teams(&self) -> &[rust_team_data::v1::Team] {
+        &self.teams[..]
+    }
+}
+
+/// Access to the Zulip API
 struct ZulipApi {
     client: Client,
     token: String,
@@ -97,6 +113,7 @@ const ZULIP_BASE_URL: &str = "https://rust-lang.zulipchat.com/api/v1";
 const BOT_EMAIL: &str = "me@ryanlevick.com"; // TODO: Change
 
 impl ZulipApi {
+    /// Create a new `ZulipApi` instance
     fn new(token: String, dry_run: bool) -> Self {
         Self {
             client: Client::new(),
@@ -105,53 +122,46 @@ impl ZulipApi {
         }
     }
 
+    /// Creates a Zulip user group with the supplied name and members
+    ///
+    /// The user group's name will be of the form T-$name. This is a
+    /// noop if the user group already exists.
     fn create_user_group(&self, name: &str, member_ids: &[usize]) -> Result<(), Error> {
+        let user_group_name = format!("T-{}", name);
         log::info!(
             "creating Zulip user group '{}' with member ids: {:?}",
-            name,
+            user_group_name,
             member_ids
         );
         if !self.dry_run {
-            let mut form = std::collections::HashMap::new();
-            form.insert("name", name.to_owned());
-            form.insert("description", "DESCRIPTION".to_owned());
-            form.insert(
-                "members",
-                format!(
-                    "[{}]",
-                    member_ids
-                        .into_iter()
-                        .map(|id| id.to_string())
-                        .collect::<Vec<_>>()
-                        .join(",")
-                ),
+            let member_ids = format!(
+                "[{}]",
+                member_ids
+                    .into_iter()
+                    .map(|id| id.to_string())
+                    .collect::<Vec<_>>()
+                    .join(",")
             );
+            let mut form = HashMap::new();
+            form.insert("name", user_group_name.clone());
+            form.insert("description", format!("The {} team", name));
+            form.insert("members", member_ids);
 
-            let mut r = self
-                .client
-                .post(&format!("{}/user_groups/create", ZULIP_BASE_URL))
-                .basic_auth(BOT_EMAIL, Some(&self.token))
-                .form(&form)
-                .send()?;
+            let mut r = self.req(reqwest::Method::POST, "/user_groups/create", Some(form))?;
             if r.status() == 400 {
                 let body = r.json::<serde_json::Value>()?;
-                let error = body
-                    .get("msg")
-                    .ok_or_else(|| {
-                        failure::format_err!("got 400 when creating user group {}: {}", name, body)
-                    })?
-                    .as_str()
-                    .ok_or_else(|| {
-                        failure::format_err!("got 400 when creating user group {}: {}", name, body)
-                    })?;
+                let err = || {
+                    failure::format_err!(
+                        "got 400 when creating user group {}: {}",
+                        user_group_name,
+                        body
+                    )
+                };
+                let error = body.get("msg").ok_or_else(err)?.as_str().ok_or_else(err)?;
                 if error.contains("already exists") {
                     return Ok(());
                 } else {
-                    return Err(failure::format_err!(
-                        "got 400 when creating user group {}: {}",
-                        name,
-                        body
-                    ));
+                    return Err(err());
                 }
             }
 
@@ -161,22 +171,43 @@ impl ZulipApi {
         Ok(())
     }
 
-    fn get_members(&self) -> Result<Vec<ZulipUser>, Error> {
+    /// Get all users of the Rust Zulip instance
+    fn get_users(&self) -> Result<Vec<ZulipUser>, Error> {
         let response = self
-            .client
-            .get(&format!("{}/users", ZULIP_BASE_URL,))
-            .basic_auth(BOT_EMAIL, Some(&self.token))
-            .send()?;
+            .req(reqwest::Method::GET, "/users", None)?
+            .error_for_status()?
+            .json::<ZulipUsers>()?
+            .members;
 
-        Ok(response.error_for_status()?.json::<ZulipUsers>()?.members)
+        Ok(response)
+    }
+
+    /// Perform a request against the Zulip API
+    fn req(
+        &self,
+        method: reqwest::Method,
+        path: &str,
+        form: Option<HashMap<&str, String>>,
+    ) -> Result<reqwest::Response, Error> {
+        let mut req = self
+            .client
+            .request(method, &format!("{}{}", ZULIP_BASE_URL, path))
+            .basic_auth(BOT_EMAIL, Some(&self.token));
+        if let Some(form) = form {
+            req = req.form(&form);
+        }
+
+        Ok(req.send()?)
     }
 }
 
+/// A collection of Zulip users
 #[derive(Deserialize)]
 struct ZulipUsers {
     members: Vec<ZulipUser>,
 }
 
+/// A single Zulip user
 #[derive(Deserialize)]
 struct ZulipUser {
     #[serde(rename = "full_name")]
