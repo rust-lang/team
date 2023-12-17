0e6dbd2adead7484f3638d581754072bb9a76b01
David Tolnay <dtolnay@gmail.com>
2023-12-17 13:08:48 -0800

    Update toml from 0.5 to 0.8

--- src/data.rs
+++ src/data.rs
@@ -1,6 +1,6 @@
 use crate::schema::{Config, List, Person, Repo, Team, ZulipGroup};
 use anyhow::{bail, Context as _, Error};
-use serde::Deserialize;
+use serde::de::DeserializeOwned;
 use std::collections::{HashMap, HashSet};
 use std::ffi::OsStr;
 use std::path::{Path, PathBuf};
@@ -61,7 +61,7 @@ impl Data {
     fn load_dir<P, T, F>(&mut self, dir: P, nested: bool, f: F) -> Result<(), Error>
     where
         P: AsRef<Path>,
-        T: for<'de> Deserialize<'de>,
+        T: DeserializeOwned,
         F: Fn(&mut Self, &str, T) -> Result<(), Error>,
         F: Clone,
     {
@@ -166,10 +166,10 @@ impl Data {
     }
 }
 
-fn load_file<T: for<'de> Deserialize<'de>>(path: &Path) -> Result<T, Error> {
-    let content =
-        std::fs::read(path).with_context(|| format!("failed to read {}", path.display()))?;
-    let parsed = toml::from_slice(&content)
-        .with_context(|| format!("failed to parse {}", path.display()))?;
+fn load_file<T: DeserializeOwned>(path: &Path) -> Result<T, Error> {
+    let content = std::fs::read_to_string(path)
+        .with_context(|| format!("failed to read {}", path.display()))?;
+    let parsed =
+        toml::from_str(&content).with_context(|| format!("failed to parse {}", path.display()))?;
     Ok(parsed)
 }
