rust-lang/sync-team

Add basic repo member permission tests

src/github/tests/test_utils.rs
@@ -1,12 +1,15 @@
 use std::collections::{HashMap, HashSet};
 
 use derive_builder::Builder;
-use rust_team_data::v1::{GitHubTeam, Person, TeamGitHub, TeamKind};
+use rust_team_data::v1;
+use rust_team_data::v1::{
+    Bot, GitHubTeam, Person, RepoMember, RepoPermission, TeamGitHub, TeamKind,
+};
 
 use crate::github::api::{
     BranchProtection, GithubRead, Repo, RepoTeam, RepoUser, Team, TeamMember, TeamPrivacy, TeamRole,
 };
-use crate::github::{api, SyncGitHub, TeamDiff};
+use crate::github::{api, RepoDiff, SyncGitHub, TeamDiff};
 
 const DEFAULT_ORG: &str = "rust-lang";
 
@@ -21,6 +24,7 @@ type UserId = u64;
 pub struct DataModel {
     people: Vec<Person>,
     teams: Vec<TeamData>,
+    repos: Vec<RepoData>,
 }
 
 impl DataModel {
@@ -44,6 +48,18 @@ impl DataModel {
             .iter_mut()
             .find(|t| t.name == name)
             .expect("Team not found")
+    }
+
+    pub fn create_repo(&mut self, repo: RepoDataBuilder) {
+        let repo = repo.build().expect("Cannot build repo");
+        self.repos.push(repo);
+    }
+
+    pub fn get_repo(&mut self, name: &str) -> &mut RepoData {
+        self.repos
+            .iter_mut()
+            .find(|r| r.name == name)
+            .expect("Repo not found")
     }
 
     /// Creates a GitHub model from the current team data mock.
@@ -88,22 +104,79 @@ impl DataModel {
             }
         }
 
+        let mut repos = HashMap::default();
+        let mut repo_members: HashMap<String, (Vec<RepoTeam>, Vec<RepoUser>)> = HashMap::default();
+
+        for repo in &self.repos {
+            repos.insert(
+                repo.name.clone(),
+                Repo {
+                    id: repos.len().to_string(),
+                    name: repo.name.clone(),
+                    org: DEFAULT_ORG.to_string(),
+                    description: repo.description.clone(),
+                    homepage: None,
+                },
+            );
+            let teams = repo
+                .teams
+                .clone()
+                .into_iter()
+                .map(|t| api::RepoTeam {
+                    name: t.name,
+                    permission: match t.permission {
+                        RepoPermission::Write => api::RepoPermission::Write,
+                        RepoPermission::Admin => api::RepoPermission::Admin,
+                        RepoPermission::Maintain => api::RepoPermission::Maintain,
+                        RepoPermission::Triage => api::RepoPermission::Triage,
+                    },
+                })
+                .collect();
+            let members = repo
+                .members
+                .clone()
+                .into_iter()
+                .map(|m| api::RepoUser {
+                    name: m.name,
+                    permission: match m.permission {
+                        RepoPermission::Write => api::RepoPermission::Write,
+                        RepoPermission::Admin => api::RepoPermission::Admin,
+                        RepoPermission::Maintain => api::RepoPermission::Maintain,
+                        RepoPermission::Triage => api::RepoPermission::Triage,
+                    },
+                })
+                .collect();
+            repo_members.insert(repo.name.clone(), (teams, members));
+        }
+
         GithubMock {
             users,
             owners: Default::default(),
             teams,
             team_memberships,
             team_invitations: Default::default(),
+            repos,
+            repo_members,
         }
     }
 
     pub fn diff_teams(&self, github: GithubMock) -> Vec<TeamDiff> {
-        let teams = self.teams.iter().map(|r| r.to_data()).collect();
-        let repos = vec![];
+        self.create_sync(github)
+            .diff_teams()
+            .expect("Cannot diff teams")
+    }
 
-        let read = Box::new(github);
-        let sync = SyncGitHub::new(read, teams, repos).expect("Cannot create SyncGitHub");
-        sync.diff_teams().expect("Cannot diff teams")
+    pub fn diff_repos(&self, github: GithubMock) -> Vec<RepoDiff> {
+        self.create_sync(github)
+            .diff_repos()
+            .expect("Cannot diff repos")
+    }
+
+    fn create_sync(&self, github: GithubMock) -> SyncGitHub {
+        let teams = self.teams.iter().map(|t| t.to_data()).collect();
+        let repos = self.repos.iter().map(|r| r.to_data()).collect();
+
+        SyncGitHub::new(Box::new(github), teams, repos).expect("Cannot create SyncGitHub")
     }
 }
 
@@ -174,6 +247,75 @@ impl TeamDataBuilder {
     }
 }
 
+#[derive(Clone, Builder)]
+#[builder(pattern = "owned")]
+pub struct RepoData {
+    name: String,
+    #[builder(default)]
+    description: Option<String>,
+    #[builder(default)]
+    bots: Vec<Bot>,
+    #[builder(default)]
+    teams: Vec<v1::RepoTeam>,
+    #[builder(default)]
+    pub members: Vec<v1::RepoMember>,
+}
+
+impl RepoData {
+    pub fn new(name: &str) -> RepoDataBuilder {
+        RepoDataBuilder::default().name(name.to_string())
+    }
+
+    pub fn add_member(&mut self, name: &str, permission: RepoPermission) {
+        self.members.push(RepoMember {
+            name: name.to_string(),
+            permission,
+        });
+    }
+
+    fn to_data(&self) -> v1::Repo {
+        let RepoData {
+            name,
+            description,
+            bots,
+            teams,
+            members,
+        } = self.clone();
+        v1::Repo {
+            org: DEFAULT_ORG.to_string(),
+            name: name.clone(),
+            description: description.unwrap_or_default(),
+            homepage: None,
+            bots,
+            teams: teams.clone(),
+            members: members.clone(),
+            branch_protections: vec![],
+        }
+    }
+}
+
+impl RepoDataBuilder {
+    pub fn team(mut self, name: &str, permission: RepoPermission) -> Self {
+        let mut teams = self.teams.clone().unwrap_or_default();
+        teams.push(v1::RepoTeam {
+            name: name.to_string(),
+            permission,
+        });
+        self.teams = Some(teams);
+        self
+    }
+
+    pub fn member(mut self, name: &str, permission: RepoPermission) -> Self {
+        let mut members = self.members.clone().unwrap_or_default();
+        members.push(v1::RepoMember {
+            name: name.to_string(),
+            permission,
+        });
+        self.members = Some(members);
+        self
+    }
+}
+
 /// Represents the state of GitHub repositories, teams and users.
 #[derive(Default)]
 pub struct GithubMock {
@@ -186,6 +328,10 @@ pub struct GithubMock {
     team_memberships: HashMap<String, HashMap<UserId, TeamMember>>,
     // Team name -> list of invited users
     team_invitations: HashMap<String, Vec<String>>,
+    // Repo name -> repo data
+    repos: HashMap<String, Repo>,
+    // Repo name -> (teams, members)
+    repo_members: HashMap<String, (Vec<RepoTeam>, Vec<RepoUser>)>,
 }
 
 impl GithubMock {
@@ -254,23 +400,37 @@ impl GithubRead for GithubMock {
             .collect())
     }
 
-    fn repo(&self, _org: &str, _repo: &str) -> anyhow::Result<Option<Repo>> {
-        todo!()
+    fn repo(&self, org: &str, repo: &str) -> anyhow::Result<Option<Repo>> {
+        assert_eq!(org, DEFAULT_ORG);
+        Ok(self.repos.get(repo).cloned())
     }
 
-    fn repo_teams(&self, _org: &str, _repo: &str) -> anyhow::Result<Vec<RepoTeam>> {
-        todo!()
+    fn repo_teams(&self, org: &str, repo: &str) -> anyhow::Result<Vec<RepoTeam>> {
+        assert_eq!(org, DEFAULT_ORG);
+        Ok(self
+            .repo_members
+            .get(repo)
+            .cloned()
+            .map(|(teams, _)| teams)
+            .unwrap_or_default())
     }
 
-    fn repo_collaborators(&self, _org: &str, _repo: &str) -> anyhow::Result<Vec<RepoUser>> {
-        todo!()
+    fn repo_collaborators(&self, org: &str, repo: &str) -> anyhow::Result<Vec<RepoUser>> {
+        assert_eq!(org, DEFAULT_ORG);
+        Ok(self
+            .repo_members
+            .get(repo)
+            .cloned()
+            .map(|(_, members)| members)
+            .unwrap_or_default())
     }
 
     fn branch_protections(
         &self,
-        _org: &str,
+        org: &str,
         _repo: &str,
     ) -> anyhow::Result<HashMap<String, (String, BranchProtection)>> {
-        todo!()
+        assert_eq!(org, DEFAULT_ORG);
+        Ok(HashMap::default())
     }
 }

rust-lang/team

sync-team/src/github/tests/test_utils.rs
@@ -1,13 +1,16 @@
 use std::collections::{HashMap, HashSet};
 
 use derive_builder::Builder;
-use rust_team_data::v1::{GitHubTeam, Person, TeamGitHub, TeamKind};
+use rust_team_data::v1;
+use rust_team_data::v1::{
+    Bot, GitHubTeam, Person, RepoMember, RepoPermission, TeamGitHub, TeamKind,
+};
 
 use crate::github::api::{
     BranchProtection, GithubRead, OrgAppInstallation, Repo, RepoAppInstallation, RepoTeam,
     RepoUser, Team, TeamMember, TeamPrivacy, TeamRole,
 };
-use crate::github::{api, SyncGitHub, TeamDiff};
+use crate::github::{api, RepoDiff, SyncGitHub, TeamDiff};
 
 const DEFAULT_ORG: &str = "rust-lang";
 
@@ -22,6 +25,7 @@ type UserId = u64;
 pub struct DataModel {
     people: Vec<Person>,
     teams: Vec<TeamData>,
+    repos: Vec<RepoData>,
 }
 
 impl DataModel {
@@ -45,6 +49,18 @@ impl DataModel {
             .iter_mut()
             .find(|t| t.name == name)
             .expect("Team not found")
+    }
+
+    pub fn create_repo(&mut self, repo: RepoDataBuilder) {
+        let repo = repo.build().expect("Cannot build repo");
+        self.repos.push(repo);
+    }
+
+    pub fn get_repo(&mut self, name: &str) -> &mut RepoData {
+        self.repos
+            .iter_mut()
+            .find(|r| r.name == name)
+            .expect("Repo not found")
     }
 
     /// Creates a GitHub model from the current team data mock.
@@ -89,22 +105,82 @@ impl DataModel {
             }
         }
 
+        let mut repos = HashMap::default();
+        let mut repo_members: HashMap<String, (Vec<RepoTeam>, Vec<RepoUser>)> = HashMap::default();
+
+        for repo in &self.repos {
+            repos.insert(
+                repo.name.clone(),
+                Repo {
+                    repo_id: repos.len() as u64,
+                    node_id: repos.len().to_string(),
+                    name: repo.name.clone(),
+                    org: DEFAULT_ORG.to_string(),
+                    description: repo.description.clone(),
+                    homepage: None,
+                    archived: false,
+                    allow_auto_merge: None,
+                },
+            );
+            let teams = repo
+                .teams
+                .clone()
+                .into_iter()
+                .map(|t| api::RepoTeam {
+                    name: t.name,
+                    permission: match t.permission {
+                        RepoPermission::Write => api::RepoPermission::Write,
+                        RepoPermission::Admin => api::RepoPermission::Admin,
+                        RepoPermission::Maintain => api::RepoPermission::Maintain,
+                        RepoPermission::Triage => api::RepoPermission::Triage,
+                    },
+                })
+                .collect();
+            let members = repo
+                .members
+                .clone()
+                .into_iter()
+                .map(|m| api::RepoUser {
+                    name: m.name,
+                    permission: match m.permission {
+                        RepoPermission::Write => api::RepoPermission::Write,
+                        RepoPermission::Admin => api::RepoPermission::Admin,
+                        RepoPermission::Maintain => api::RepoPermission::Maintain,
+                        RepoPermission::Triage => api::RepoPermission::Triage,
+                    },
+                })
+                .collect();
+            repo_members.insert(repo.name.clone(), (teams, members));
+        }
+
         GithubMock {
             users,
             owners: Default::default(),
             teams,
             team_memberships,
             team_invitations: Default::default(),
+            repos,
+            repo_members,
         }
     }
 
     pub fn diff_teams(&self, github: GithubMock) -> Vec<TeamDiff> {
-        let teams = self.teams.iter().map(|r| r.to_data()).collect();
-        let repos = vec![];
+        self.create_sync(github)
+            .diff_teams()
+            .expect("Cannot diff teams")
+    }
 
-        let read = Box::new(github);
-        let sync = SyncGitHub::new(read, teams, repos).expect("Cannot create SyncGitHub");
-        sync.diff_teams().expect("Cannot diff teams")
+    pub fn diff_repos(&self, github: GithubMock) -> Vec<RepoDiff> {
+        self.create_sync(github)
+            .diff_repos()
+            .expect("Cannot diff repos")
+    }
+
+    fn create_sync(&self, github: GithubMock) -> SyncGitHub {
+        let teams = self.teams.iter().map(|t| t.to_data()).collect();
+        let repos = self.repos.iter().map(|r| r.to_data()).collect();
+
+        SyncGitHub::new(Box::new(github), teams, repos).expect("Cannot create SyncGitHub")
     }
 }
 
@@ -176,6 +252,78 @@ impl TeamDataBuilder {
     }
 }
 
+#[derive(Clone, Builder)]
+#[builder(pattern = "owned")]
+pub struct RepoData {
+    name: String,
+    #[builder(default)]
+    description: Option<String>,
+    #[builder(default)]
+    bots: Vec<Bot>,
+    #[builder(default)]
+    teams: Vec<v1::RepoTeam>,
+    #[builder(default)]
+    pub members: Vec<v1::RepoMember>,
+}
+
+impl RepoData {
+    pub fn new(name: &str) -> RepoDataBuilder {
+        RepoDataBuilder::default().name(name.to_string())
+    }
+
+    pub fn add_member(&mut self, name: &str, permission: RepoPermission) {
+        self.members.push(RepoMember {
+            name: name.to_string(),
+            permission,
+        });
+    }
+
+    fn to_data(&self) -> v1::Repo {
+        let RepoData {
+            name,
+            description,
+            bots,
+            teams,
+            members,
+        } = self.clone();
+        v1::Repo {
+            org: DEFAULT_ORG.to_string(),
+            name: name.clone(),
+            description: description.unwrap_or_default(),
+            homepage: None,
+            bots,
+            teams: teams.clone(),
+            members: members.clone(),
+            branch_protections: vec![],
+            archived: false,
+            private: false,
+            auto_merge_enabled: false,
+        }
+    }
+}
+
+impl RepoDataBuilder {
+    pub fn team(mut self, name: &str, permission: RepoPermission) -> Self {
+        let mut teams = self.teams.clone().unwrap_or_default();
+        teams.push(v1::RepoTeam {
+            name: name.to_string(),
+            permission,
+        });
+        self.teams = Some(teams);
+        self
+    }
+
+    pub fn member(mut self, name: &str, permission: RepoPermission) -> Self {
+        let mut members = self.members.clone().unwrap_or_default();
+        members.push(v1::RepoMember {
+            name: name.to_string(),
+            permission,
+        });
+        self.members = Some(members);
+        self
+    }
+}
+
 /// Represents the state of GitHub repositories, teams and users.
 #[derive(Default)]
 pub struct GithubMock {
@@ -188,6 +336,10 @@ pub struct GithubMock {
     team_memberships: HashMap<String, HashMap<UserId, TeamMember>>,
     // Team name -> list of invited users
     team_invitations: HashMap<String, Vec<String>>,
+    // Repo name -> repo data
+    repos: HashMap<String, Repo>,
+    // Repo name -> (teams, members)
+    repo_members: HashMap<String, (Vec<RepoTeam>, Vec<RepoUser>)>,
 }
 
 impl GithubMock {
@@ -267,23 +419,37 @@ impl GithubRead for GithubMock {
             .collect())
     }
 
-    fn repo(&self, _org: &str, _repo: &str) -> anyhow::Result<Option<Repo>> {
-        todo!()
+    fn repo(&self, org: &str, repo: &str) -> anyhow::Result<Option<Repo>> {
+        assert_eq!(org, DEFAULT_ORG);
+        Ok(self.repos.get(repo).cloned())
     }
 
-    fn repo_teams(&self, _org: &str, _repo: &str) -> anyhow::Result<Vec<RepoTeam>> {
-        todo!()
+    fn repo_teams(&self, org: &str, repo: &str) -> anyhow::Result<Vec<RepoTeam>> {
+        assert_eq!(org, DEFAULT_ORG);
+        Ok(self
+            .repo_members
+            .get(repo)
+            .cloned()
+            .map(|(teams, _)| teams)
+            .unwrap_or_default())
     }
 
-    fn repo_collaborators(&self, _org: &str, _repo: &str) -> anyhow::Result<Vec<RepoUser>> {
-        todo!()
+    fn repo_collaborators(&self, org: &str, repo: &str) -> anyhow::Result<Vec<RepoUser>> {
+        assert_eq!(org, DEFAULT_ORG);
+        Ok(self
+            .repo_members
+            .get(repo)
+            .cloned()
+            .map(|(_, members)| members)
+            .unwrap_or_default())
     }
 
     fn branch_protections(
         &self,
-        _org: &str,
+        org: &str,
         _repo: &str,
     ) -> anyhow::Result<HashMap<String, (String, BranchProtection)>> {
-        todo!()
+        assert_eq!(org, DEFAULT_ORG);
+        Ok(HashMap::default())
     }
 }