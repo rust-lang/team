d90a5f80488f3dec05d4c77db3d31b249766da36
Tshepang Mbambo <tshepang.mbambo@ferrous-systems.com>
2025-06-11 10:39:43 +0200

    cargo clippy --fix

--- src/main.rs
+++ src/main.rs
@@ -165,7 +165,7 @@ impl FromStr for DataSource {
                 let path = PathBuf::from(path);
                 if path.is_dir() {
                     Ok(Self::Prebuilt {
-                        path: PathBuf::from(path),
+                        path: path,
                     })
                 } else {
                     Err(
@@ -201,7 +201,7 @@ fn main() {
     env.init();
 
     if let Err(e) = run() {
-        error!("{:?}", e);
+        error!("{e:?}");
         std::process::exit(1);
     }
 }
@@ -257,7 +257,7 @@ fn run() -> Result<(), Error> {
                 bail!("person already in the repo: {}", github_name);
             }
 
-            let file = format!("people/{}.toml", github_name);
+            let file = format!("people/{github_name}.toml");
             std::fs::write(
                 &file,
                 toml::to_string_pretty(&PersonToAdd {
@@ -278,7 +278,7 @@ fn run() -> Result<(), Error> {
                 .as_bytes(),
             )?;
 
-            info!("written data to {}", file);
+            info!("written data to {file}");
         }
         Cli::StaticApi { ref dest } => {
             let dest = PathBuf::from(dest);
@@ -312,7 +312,7 @@ fn run() -> Result<(), Error> {
                 }
             }
             if let Email::Present(email) = person.email() {
-                println!("email: {}", email);
+                println!("email: {email}");
             }
             println!();
 
@@ -352,7 +352,7 @@ fn run() -> Result<(), Error> {
                 println!("  (none)");
             } else {
                 for (repo, perms) in bors_permissions {
-                    println!("  - {}", repo);
+                    println!("  - {repo}");
                     if perms.review() {
                         println!("    - review");
                     }
@@ -373,7 +373,7 @@ fn run() -> Result<(), Error> {
                 println!("  (none)");
             } else {
                 for key in other_permissions {
-                    println!("  - {}", key);
+                    println!("  - {key}");
                 }
             }
         }
@@ -399,7 +399,7 @@ fn run() -> Result<(), Error> {
                 }
                 println!("{} ({}):", team.name(), team.kind());
                 if let Some(parent) = team.subteam_of() {
-                    println!("  parent team: {}", parent);
+                    println!("  parent team: {parent}");
                 }
 
                 println!("  members: ");
@@ -418,7 +418,7 @@ fn run() -> Result<(), Error> {
             let mut emails = list.emails().iter().collect::<Vec<_>>();
             emails.sort();
             for email in emails {
-                println!("{}", email);
+                println!("{email}");
             }
         }
         Cli::DumpWebsite => {
@@ -456,7 +456,7 @@ fn run() -> Result<(), Error> {
                 .collect::<Vec<_>>();
             allowed.sort_unstable();
             for github_username in &allowed {
-                println!("{}", github_username);
+                println!("{github_username}");
             }
         }
         Cli::DumpIndividualAccess { group_by } => {
@@ -526,9 +526,9 @@ fn run() -> Result<(), Error> {
         Cli::Sync(opts) => {
             if let Err(err) = perform_sync(opts, data) {
                 // Display shows just the first element of the chain.
-                error!("failed: {}", err);
+                error!("failed: {err}");
                 for cause in err.chain().skip(1) {
-                    error!("caused by: {}", cause);
+                    error!("caused by: {cause}");
                 }
                 std::process::exit(1);
             }
